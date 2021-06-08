1a58b6d24c1cf2f322684ea04374633f1c3eb830
Addison Wessel <addisonwessel@gmail.com>
2021-06-08 16:03:18 -0400

    Added collision to player

--- Assets/GridMovement.cs
+++ Assets/GridMovement.cs
@@ -6,7 +6,8 @@ public class GridMovement : MonoBehaviour
 {
     public float gridSize = 1; //should be ~0.48
     public float timeToMove = 0.2f;
-    public int startX = 15, startY = 27;
+    public int startGridX = 15, startGridY = 27;
+    public float startX, startY;
     public TextAsset collisionMap;
 
     //direction constants
@@ -18,6 +19,7 @@ public class GridMovement : MonoBehaviour
     private int heading = NORTH;
     private int gridX, gridY;
     private bool[,] collisions;
+    private bool[] canMove = new bool[4] { false, false, false, false };
     private bool isMoving = false;
     private Vector3 origPos, targetPos;
     private Quaternion origHeading, targetHeading;
@@ -27,64 +29,81 @@ public class GridMovement : MonoBehaviour
         //reading collision file and setting up collision matrix
         List<string> eachLine = new List<string>();
         eachLine.AddRange(collisionMap.text.Split("\n"[0]));
+        gridX = startGridX;
+        gridY = startGridY;
         
-        /*
-         * note on indexing: in Unity, +y is up and -y is down; 
-         * in collisions[x,y], +y is down and -y is up. this sucks but i'm stubborn. 
-         */
         collisions = new bool[eachLine[0].Length, eachLine.Count]; 
         for (int i = 0; i < eachLine.Count; i++)
         {
+            Debug.Log("line " + i + ": " + eachLine[i]);
             for(int j = 0; j < eachLine[i].Length; j++)
             {
-                collisions[j, i] = eachLine[i][j] == '0';
+                collisions[j, eachLine.Count - (i+1)] = eachLine[i][j] == '0'; //inserting lines in inverse order, to keep +y facing North. 
             }
         }
+        transform.position = new Vector3(gridSize * (gridX - 0.5f * collisions.GetLength(0)) + startX, gridSize * (gridY - 0.5f * collisions.GetLength(1)) + startY, 0);
+        updateCanMove();
     }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W) && !isMoving && heading != SOUTH)
+        if (Input.GetKey(KeyCode.W) && !isMoving && heading != SOUTH && canMove[NORTH])
         {
             StartCoroutine(MovePlayer(NORTH));
-        } else if (Input.GetKey(KeyCode.A) && !isMoving && heading != EAST)
+        } else if (Input.GetKey(KeyCode.A) && !isMoving && heading != EAST && canMove[WEST])
         {
             StartCoroutine(MovePlayer(WEST));
-        } else if (Input.GetKey(KeyCode.S) && !isMoving && heading != NORTH)
+        } else if (Input.GetKey(KeyCode.S) && !isMoving && heading != NORTH && canMove[SOUTH])
         {
             StartCoroutine(MovePlayer(SOUTH));
-        } else if (Input.GetKey(KeyCode.D) && !isMoving && heading != WEST)
+        } else if (Input.GetKey(KeyCode.D) && !isMoving && heading != WEST && canMove[EAST])
         { 
             StartCoroutine(MovePlayer(EAST));
-        } else if(!isMoving)
+        } else if(!isMoving && canMove[heading])
         {
             StartCoroutine(MovePlayer(heading));
         }
+    }
 
+    private void updateCanMove()
+    {
+        canMove[NORTH] = collisions[gridX, gridY + 1];
+        canMove[EAST] = collisions[gridX + 1, gridY];
+        canMove[WEST] = collisions[gridX - 1, gridY];
+        canMove[SOUTH] = collisions[gridX, gridY - 1];
     }
 
     private IEnumerator MovePlayer(int newHeading)
     {
+        isMoving = true;
         Vector3 direction;
+
+        //updating direction and grid position
         switch (newHeading)
         {
             case NORTH:
                 direction = Vector3.up;
+                gridY++;
                 break;
             case SOUTH:
                 direction = Vector3.down;
+                gridY--;
                 break;
             case EAST:
                 direction = Vector3.right;
+                gridX++;
                 break;
             case WEST:
                 direction = Vector3.left;
+                gridX--;
                 break;
             default:
                 direction = Vector3.up;
+                gridY++;
                 break;
         }
-        isMoving = true;
+        updateCanMove();
+        //Debug.Log("Grid Pos: [" + gridX + ", " + gridY + "]");
         heading = newHeading;
         float elapsedTime = 0;
         origPos = transform.position;
